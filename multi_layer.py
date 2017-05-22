8dd23c3eaa411d0a66cd6b94caac24685b691dfe
Beomjin Ahn <expect017@naver.com>
2017-05-22 14:58:29 +0900

    Auto stash before merge of "master" and "origin/master"

--- multi_layer.py
+++ multi_layer.py
@@ -42,6 +42,11 @@ def train(node_num, X, y, syn):
     return ret
 
 def classify(node_num, X, syn):
+    def nonlin(x, deriv=False):
+        if (deriv == True):
+            return x * (1 - x)
+        return 1 / (1 + np.exp(-x))
+
     l = [[] for _ in range(len(node_num))]
     l[0] = X
     for i in range(0,len(node_num)-1):
@@ -59,87 +64,91 @@ def classify(node_num, X, syn):
         if softmax[i] == max(softmax):
             return i
 
-np.random.seed(1)
+def __init__(self):
+    np.random.seed(1)
 
-#init spark
-conf = (SparkConf()
-        .setAppName("SPARK_ANN")
-        .setMaster("spark://192.168.0.3:7077"))
-#        .setMaster("local[*]"))
-sc = SparkContext(conf=conf)
+    #init spark
+    conf = (SparkConf()
+            .setAppName("SPARK_ANN")
+            .setMaster("spark://192.168.0.3:7077"))
+    #        .setMaster("local[*]"))
+    sc = SparkContext(conf=conf)
 
-#load from file
-if platform.system() == 'Linux':
-    path = '/home/master/Downloads/WISDM_at_v2.0/WISDM_at_v2.0_raw.txt'
-elif platform.system() == 'Windows':
-    path = 'C:\Users\KUsch\Downloads\WISDM_at_v2.0\WISDM_at_v2.0_raw.txt'
-else:
-    path = '/Users/Abj/Downloads/WISDM_at_v2.0/WISDM_at_v2.0_raw.txt'
+    #load from file
+    if platform.system() == 'Linux':
+        path = '/home/master/Downloads/WISDM_at_v2.0/WISDM_at_v2.0_raw.txt'
+    elif platform.system() == 'Windows':
+        path = 'C:\Users\KUsch\Downloads\WISDM_at_v2.0\WISDM_at_v2.0_raw.txt'
+    else:
+        path = '/Users/Abj/Downloads/WISDM_at_v2.0/WISDM_at_v2.0_raw.txt'
 
-csv = sc.textFile(path,24)
-data = csv.filter(lambda line: line[-1]==';')\
-    .map(lambda line: (line.split(",")))\
-    .filter(lambda line: len(line)==6)
-#replace exercise name to num
-def replace(line):
-    exercise = {"Walking" : 0,"Jogging" : 1,"Stairs" : 2,"Sitting" : 3,"Standing" : 4,"LyingDown" : 5}
-    line[1] = exercise[line[1]]
-    line[5] = line[5].replace(";","")
-    return line
-data = data.map(lambda line: replace(line))
-#form change to (X,Y,Z), result
-def change(line):
-    temp = [0.0 for _ in range(6)]
-    temp[line[1]] = 1.0
-    return ((float(line[3]),float(line[4]),float(line[5])),temp)
-train_data = data.map(lambda line: change(line))
-#shuffle rdd
-train_data = train_data.repartition(24)
+    csv = sc.textFile(path,24)
+    data = csv.filter(lambda line: line[-1]==';')\
+        .map(lambda line: (line.split(",")))\
+        .filter(lambda line: len(line)==6)
+    #replace exercise name to num
+    def replace(line):
+        exercise = {"Walking" : 0,"Jogging" : 1,"Stairs" : 2,"Sitting" : 3,"Standing" : 4,"LyingDown" : 5}
+        line[1] = exercise[line[1]]
+        line[5] = line[5].replace(";","")
+        return line
+    data = data.map(lambda line: replace(line))
+    #form change to (X,Y,Z), result
+    def change(line):
+        temp = [0.0 for _ in range(6)]
+        temp[line[1]] = 1.0
+        return ((float(line[3]),float(line[4]),float(line[5])),temp)
+    train_data = data.map(lambda line: change(line))
+    #shuffle rdd
+    train_data = train_data.repartition(24)
 
-#first = 3, last = 6
-node_num = [3,1000,6]
-num_of_train = 100
-batch_size = 100
+    #first = 3, last = 6
+    node_num = [3,500,500,500,500,500,6]
+    num_of_train = 50
+    batch_size = 3000
 
-syn = []
-for i in range(0,len(node_num)-1):
-    syn.append(np.random.random((node_num[i], node_num[i+1])))
-
-train_data = train_data.zipWithIndex()
-
-#split test and train data
-test_ratio = 10
-rdd_size = train_data.count()
-test_data = train_data.filter(lambda (data,index): index>rdd_size*(100-test_ratio)/100)
-train_data = train_data.filter(lambda (data,index): index<=rdd_size*(100-test_ratio)/100)
-
-print "Start training >>"
-print "node_num = "+str(node_num)+", num_of_train = "+str(num_of_train)+", batch_size = "+str(batch_size)
-for loop in range(0,num_of_train):
-    print "train loop = ", loop+1
-    train_batch = train_data.filter(lambda (data,index): index%batch_size == loop%batch_size)
-    rdd = train_batch.map(lambda (data,index): train(node_num,\
-                                           np.expand_dims(data[0],axis=0),\
-                                           np.expand_dims(data[1],axis=0),\
-                                           syn))
-    delta = [0 for _ in range(len(node_num))]
+    syn = []
     for i in range(0,len(node_num)-1):
-        delta[i] = rdd.map(lambda x: x[i]).mean()
-    error = rdd.map(lambda x: x[-1]).mean()
+        syn.append(np.random.random((node_num[i], node_num[i+1])))
 
-    #alpha if learning rate
-    alpha = 0.1
-    for i in range(0,len(node_num)-1):
-        syn[i] -= alpha * delta[i]
-    
-    print error
+    train_data = train_data.zipWithIndex()
 
-num_of_test = test_data.count()
+    #split test and train data
+    test_ratio = 0.1
+    rdd_size = train_data.count()
+    test_data = train_data.filter(lambda (data,index): index>rdd_size*(100-test_ratio)/100)
+    train_data = train_data.filter(lambda (data,index): index<=rdd_size*(100-test_ratio)/100)
 
-print "Start testing >>"
-test_result = test_data.filter(lambda (data, index): test_data[1][classify(node_num, np.expand_dims(data[0],axis=0), syn)] == 1.0)
-succeed = test_result.count()
+    print "Start training >>"
+    print "node_num = "+str(node_num)+", num_of_train = "+str(num_of_train)+", batch_size = "+str(batch_size)
+    for loop in range(0,num_of_train):
+        print "train loop = ", loop+1
+        train_batch = train_data.filter(lambda (data,index): index%batch_size == loop%batch_size)
+        rdd = train_batch.map(lambda (data,index): train(node_num,\
+                                               np.expand_dims(data[0],axis=0),\
+                                               np.expand_dims(data[1],axis=0),\
+                                               syn))
+        delta = [0 for _ in range(len(node_num))]
+        for i in range(0,len(node_num)-1):
+            delta[i] = rdd.map(lambda x: x[i]).mean()
+        error = rdd.map(lambda x: x[-1]).mean()
 
-print "correct : ",succeed
-print "wrong : ", (num_of_test - succeed)
-print "accurancy : ", succeed*100.0/num_of_test,"%"
+        #alpha if learning rate
+        alpha = 0.1
+        for i in range(0,len(node_num)-1):
+            syn[i] -= alpha * delta[i]
+
+        print error
+
+    num_of_test = test_data.count()
+
+    print "Start testing >>"
+    print "num_of_test = "+str(num_of_test)
+    test_result = test_data.filter(lambda (data, index):
+                                   data[1][classify(node_num, np.expand_dims(data[0],axis=0), syn)]
+                                   == 1.0)
+    succeed = test_result.count()
+
+    print "correct : ",succeed
+    print "wrong : ", (num_of_test - succeed)
+    print "accurancy : ", succeed*100.0/num_of_test,"%"
